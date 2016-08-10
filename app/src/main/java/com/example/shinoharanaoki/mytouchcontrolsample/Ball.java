f4c45e6f9e53c64e0b6871a9bb4dbfcf1bf49b8d
cocodoba <tpdhp991>
2016-08-10 14:51:48 +0900

    ThreeBallsViewにBallオブジェクトを３つ描画できた

--- app/src/main/java/com/example/shinoharanaoki/mytouchcontrolsample/Ball.java
+++ app/src/main/java/com/example/shinoharanaoki/mytouchcontrolsample/Ball.java
@@ -7,6 +7,17 @@ public class Ball {
     private static final String TAG = Ball.class.getSimpleName();
     private final Ball self = this;
 
+    public float cx;    // 図形を描画する X 座標    // (1)
+    public float cy;    // 図形を描画する Y 座標    // (2)
+
+    public float radius;    // 円の半径    // (3)
+
+    public Ball(float x, float y, float r){
+        cx = x;
+        cy = y;
+        radius = r;
+    }
+
 
 
 }
--- app/src/main/java/com/example/shinoharanaoki/mytouchcontrolsample/ThreeBallsView.java
+++ app/src/main/java/com/example/shinoharanaoki/mytouchcontrolsample/ThreeBallsView.java
@@ -14,12 +14,13 @@ public class ThreeBallsView extends View{
     private static final String TAG = ThreeBallsView.class.getSimpleName();
     private final ThreeBallsView self = this;
 
-    private float cx;    // 図形を描画する X 座標    // (1)
-    private float cy;    // 図形を描画する Y 座標    // (2)
-    private float radius;    // 円の半径    // (3)
+    private float touch_x;    // 画面のタッチされた X 座標    // (1)
+    private float touch_y;    // 画面のタッチされた Y 座標    // (2)
 
     private Canvas canvas;
     private Paint paint;
+
+    private Ball[] balls;
 
     public ThreeBallsView(Context context) {
         super(context);
@@ -33,16 +34,29 @@ public class ThreeBallsView extends View{
         paint.setColor(Color.BLUE);    // (4)
         paint.setStyle(Paint.Style.FILL);    // (5)
 
-        // 丸を描画する初期値を設定する
-        cx = 100;
-        cy = 200;
-        radius = 50;
+
+        balls = new Ball[3];
+        float radius = 50;
+        float init_x = 100;
+        float init_y = 100;
+        for (int i=0;i<3;i++) {
+            balls[i] = new Ball(init_x, init_y, radius);
+            init_x += 100;
+            init_y += 50;
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         // 円を描画する
-        canvas.drawCircle(cx, cy, radius, paint);    // (6)
+
+        for(int i=0;i<balls.length;i++) {
+
+            float ball_x = balls[i].cx;
+            float ball_y = balls[i].cy;
+            float radius = balls[i].radius;
+            canvas.drawCircle(ball_x, ball_y, radius, paint);  // (6)
+        }
     }
 
     @Override
@@ -54,8 +68,8 @@ public class ThreeBallsView extends View{
                 break;
 
             case MotionEvent.ACTION_MOVE:    // 指を動かしている    // (9)
-                cx = event.getX();    // (10)
-                cy = event.getY();    // (11)
+                touch_x = event.getX();    // (10)
+                touch_y = event.getY();    // (11)
                 break;
 
             case MotionEvent.ACTION_UP:        // 指を離した    // (12)
